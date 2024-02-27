1714481957c56ea068824db18dcd477d2474497d
gokul-ram-j <gokulram221@gmail.com>
2024-02-27 12:34:41 +0530

    27-02

--- recipe book/bootstrap/js/app.js
+++ recipe book/bootstrap/js/app.js
@@ -1,22 +1,31 @@
 var myHeaders = new Headers();
 myHeaders.append("apikey", "SOAVSaquauSGJBIIoW36Z9aNDuvhbw3o");
-
+let sortDirection="asc"
+let sort="popularity"
+let query="chicken"
+let offset="10"
+let number="50"
+let intolerances=`Wheat`
 var requestOptions = {
   method: 'GET',
   redirect: 'follow',
   headers: myHeaders
 };
 
-let sortDirection="asc"
-let sort="popularity"
-let query="tomato"
-let offset=0
-let number=50
-let intolerances=`Peanut
-Seafood,
-Sesame,
-Shellfish`
-fetch(`https://api.apilayer.com/spoonacular/food/ingredients/search?sortDirection=${sortDirection}&sort=${sort}&query=${query}&offset=${offset}&number=${number}&intolerances=${intolerances}`, requestOptions)
+//  -------------------------- Restarent or hetel foods -----------------------------------------
+fetch(`https://api.apilayer.com/spoonacular/food/menuItems/search?query=${query}`, requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  
+  fetch(`https://api.apilayer.com/spoonacular/food/ingredients/search?sortDirection=${sortDirection}&sort=${sort}&query=${query}&offset=${offset}&number=${number}&intolerances=${intolerances}`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
