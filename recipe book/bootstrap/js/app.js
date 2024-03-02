3135c48f16e5acc9a0ad2659e5f68f070017f80e
gokul-ram-j <gokulram221@gmail.com>
2024-03-02 23:01:12 +0530

    02-03

--- recipe book/bootstrap/js/app.js
+++ recipe book/bootstrap/js/app.js
@@ -1,91 +1,101 @@
-let Avail_opt=["All Recipes","fruit Salads","fruit Salads","fruit Salads","Chicken","Beef","Others..","Western","Chinese","Japan","Malasiya","Korean"]
-let navopt=document.querySelector(".navbar")
-navopt.addEventListener("click",(e)=>{
-  user_opt=e.target.innerText
-  if(Avail_opt.includes(user_opt)){
-    console.log(user_opt)
+
+
+let app_id="835abe5b"
+let app_key="324162b9baaf575256e1bbef92a86ebb"
+let recipe_container=document.querySelector(".recipe-container")
+let form=document.querySelector(".Container")
+let recipe_list=[]
+let food_type;
+form.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  let fooditem=document.querySelector(".fooditem")
+  if(fooditem.value==""){
+    alert("Please enter a food item")
+  }
+  else{
+    food_type=fooditem.value
+    recipeResponse()
   }
 })
+async function recipeResponse(){
+    fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=835abe5b&app_key=324162b9baaf575256e1bbef92a86ebb&q=${food_type}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        // console.log(data)
+        
+        for(id in data.hits){
+            // console.log(data.hits[id].recipe.label)
+            // console.log(data.hits[id].recipe)
+            recipe_list.push(data.hits[id].recipe)
+          let elem=
+          `
+          <div class="col-md-6 col-lg-4 col-sm-6 ">
+          <div id="${id}" class="recipe">
+            <img src="${data.hits[id].recipe.image}" alt="food img">
+            <div class="context-info">
+              <h2>${data.hits[id].recipe.label}</h2>
+             <button onclick="showrecipe(this)"><a>View Recipe <i class="fa fa-external-link" aria-hidden="true"></i></a></button>
+            </div>
+          </div>
+        </div>
+          `
+          recipe_container.innerHTML+=elem
+      }
+      // console.log(recipe_list)
+      // console.log(recipe_list.length)
+
+    })
+    
+    
+  }
+  
 
 
 
+// -----------------------------------------recipe.html----------------------------------------------
+let recipe_ingredients_container=document.querySelector(".ingredient-container ")
+let procedure_container=document.querySelector(".procedure-container")
+
+function showprocedure(ingredientlist,procedurelist){
+  recipe_ingredients_container.innerHTML=""
+    for(item of ingredientlist){
+      // console.log(item)
+      let elem=
+      `
+      <div class="col-md-6 col-lg-4 col-sm-6 ">
+          <div class="ingredient">
+            <img src="${item.image}" alt="food img">
+            <div class="ingredient-info">
+              <h4>${item.food}</h4>
+              <p>${item.text}</p>
+            </div>
+          </div>
+        </div>
+      
+      `
+      recipe_ingredients_container.innerHTML+=elem
+    }
+    let step=1
+    procedure_container.innerHTML=""
+    for(steps of procedurelist){
+      let elem=
+      `
+      <P><span class="indicator">step ${step}</span>: ${steps}</P>
+      `
+      step+=1
+      procedure_container.innerHTML+=elem
+    }
+    document.querySelector("#ingredient-container").scrollIntoView({behavior:'smooth'})
+}
 
 
 
+function showrecipe(clickedrecipe){
+  let recipe_id=clickedrecipe.parentElement.parentElement.id
+  // console.log(recipe_list[recipe_id].ingredientLines)
+  // console.log(recipe_list[recipe_id].ingredients)
+  // console.log(recipe_list[recipe_id].ingredients[0])
+  showprocedure(recipe_list[recipe_id].ingredients,recipe_list[recipe_id].ingredientLines)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ------------------------------------------------------------------------------------
-
-
-var myHeaders = new Headers();
-myHeaders.append("apikey", "SOAVSaquauSGJBIIoW36Z9aNDuvhbw3o");
-let sortDirection="asc"
-let sort="popularity"
-let query="rice"
-let offset="30"
-let number="50"
-let intolerances=`Wheat`
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
-
-//  -------------------------- Restarent or hetel foods -----------------------------------------
-fetch(`https://api.apilayer.com/spoonacular/food/menuItems/search?query=${query}`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
- 
-
-fetch(`https://api.apilayer.com/spoonacular/food/menuItems/search?query=fruitsalad`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-
-// console.log("-------------------------------------------------------------------------------------------------------------------------")
-
-
-// fetch("https://api.apilayer.com/spoonacular/recipes/complexSearch?query=chickenrice", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
-// fetch("www.themealdb.com/api/json/v1/1/search.php?i=rice", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
-
-let apiId="835abe5b"
-let apiKey="324162b9baaf575256e1bbef92a86ebb"
-let url="https://api.edamam.com/api/recipes/v2?api"
-fetch()
 
